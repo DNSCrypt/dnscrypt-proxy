eac8732b2b9c0506699354cc0481c5819a04f945
Frank Denis <github@pureftpd.org>
2018-02-22 14:48:08 +0100

    Log servers returning SERVFAIL

--- dnscrypt-proxy/proxy.go
+++ dnscrypt-proxy/proxy.go
@@ -314,6 +314,7 @@ func (proxy *Proxy) processIncomingQuery(serverInfo *ServerInfo, clientProto str
 			return
 		}
 		if rcode := Rcode(response); rcode == 2 || rcode == 5 { // SERVFAIL / REFUSED
+			dlog.Infof("Server [%v] returned error code [%v] -- It might be experiencing upstream connectivity issues", serverInfo.Name, rcode)
 			serverInfo.noticeFailure(proxy)
 		} else {
 			serverInfo.noticeSuccess(proxy)
