e20c980b1540bc1220f65e192b7074693d4453bc
Frank Denis <github@pureftpd.org>
2018-04-10 20:04:03 +0200

    Use CMD.EXE /c to run Windows commands
    That sounds very weird to me, but @mazesy said it was the right
    thing to do.

--- windows/service-restart.bat
+++ windows/service-restart.bat
@@ -1,6 +1,8 @@
-@SFC 2>&1 | FIND /i "/SCANNOW" >NUL
-@IF %ERRORLEVEL% NEQ 0 GOTO ELEVATE
-@GOTO ADMINTASKS
+@ECHO OFF
+
+SFC 2>&1 | FIND /i "/SCANNOW" >NUL
+IF %ERRORLEVEL% NEQ 0 GOTO ELEVATE
+GOTO ADMINTASKS
 
 :ELEVATE
 ECHO Elevated privileges are temporarily required, just to register or remove the dnscrypt-proxy service
@@ -10,12 +12,12 @@ EXIT
 
 :ADMINTASKS
 
-@CD /d %~dp0
+CD /d %~dp0
 
-.\dnscrypt-proxy.exe -service stop
-.\dnscrypt-proxy.exe -service start
+CMD.EXE /c "dnscrypt-proxy.exe -service stop"
+CMD.EXE /c "dnscrypt-proxy.exe -service start"
 
-@ECHO ""
-@SET /P _=Thank you for using dnscrypt-proxy! Hit [RETURN] to finish
+ECHO ""
+SET /P _=Thank you for using dnscrypt-proxy! Hit [RETURN] to finish
 
 EXIT
