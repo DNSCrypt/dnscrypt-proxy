35ecf1707ca6f77850f4b0902efc29d48ae878dd
gdm85 <gdm85@users.noreply.github.com>
2018-04-14 17:44:52 +0200

    Add support for visualizing DNS stamps

--- dnscrypt-stamper/main.go
+++ dnscrypt-stamper/main.go
@@ -17,10 +17,12 @@ var (
 	hashesStr     string
 	doh, dnscrypt bool
 	dnssec        bool
-	noLogs        bool
+	noLog         bool
 	noFilter      bool
 	port          uint
 )
+
+const exampleStamp = `sdns://AQcAAAAAAAAACTEyNy4wLjAuMSDDhGvyS56TymQnTA7GfB7MXgJP_KzS10AZNQ6B_lRq5BkyLmRuc2NyeXB0LWNlcnQubG9jYWxob3N0`
 
 func init() {
 	flag.BoolVar(&doh, "doh", false, "create a DNS-over-HTTPS stamp")
@@ -35,19 +37,84 @@ func init() {
 	flag.StringVar(&stamp.Path, "path", "/dns-query", "path for DNS-over-TLS queries")
 
 	flag.BoolVar(&dnssec, "dnssec", true, "enforce DNSSEC")
-	flag.BoolVar(&noLogs, "no-logs", true, "enforce no logs")
-	flag.BoolVar(&noFilter, "no-filter", true, "enforce no filter")
+	flag.BoolVar(&noLog, "no-log", false, "enforce no logs")
+	flag.BoolVar(&noFilter, "no-filter", false, "enforce no filter")
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of dnscrypt-stamper:\n")
+		flag.PrintDefaults()
+		fmt.Fprintf(os.Stderr, "\nSpecify one or more DNS stamps on command-line to visualize them:\n")
+		fmt.Fprintf(os.Stderr, "\ndnscrypt-stamper %s\n", exampleStamp)
+	}
 }
 
 func main() {
+	// display help if no arguments were specified
+	if len(os.Args) == 1 {
+		// calling Usage() will exit with code 2
+		flag.Usage()
+	}
+
 	flag.Parse()
+
+	nonParsed := flag.Args()
+	if len(nonParsed) != 0 {
+		for _, stampStr := range nonParsed {
+			stamp, err := stamps.NewServerStampFromString(stampStr)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+				os.Exit(2)
+			}
+
+			// print out common fields
+			fmt.Printf(" ---> %s:\n", stamp.String())
+			fmt.Println("\tproto =", stamp.Proto.String())
+
+			if stamp.Props&stamps.ServerInformalPropertyDNSSEC != 0 {
+				fmt.Println("\tdnssec = yes")
+			} else {
+				fmt.Println("\tdnssec = no")
+			}
+			if stamp.Props&stamps.ServerInformalPropertyNoLog != 0 {
+				fmt.Println("\tno-log = yes")
+			} else {
+				fmt.Println("\tno-log = no")
+			}
+			if stamp.Props&stamps.ServerInformalPropertyNoFilter != 0 {
+				fmt.Println("\tno-filter = yes")
+			} else {
+				fmt.Println("\tnofilter = no")
+			}
+			fmt.Println("\tip =", stamp.ServerAddrStr)
+
+			if stamp.Proto == stamps.StampProtoTypeDNSCrypt {
+				fmt.Println("\tprovider-public-key =", formatHex(stamp.ServerPk))
+				fmt.Println("\tprovider-name =", stamp.ProviderName)
+			} else if stamp.Proto == stamps.StampProtoTypeDoH {
+				fmt.Println("\thost =", stamp.ProviderName)
+				var hashes []string
+				for _, hBytes := range stamp.Hashes {
+					hashes = append(hashes, formatHex(hBytes))
+				}
+				if len(hashes) != 0 {
+					fmt.Println("\thashes =", strings.Join(hashes, ","))
+				}
+				fmt.Println("\tpath =", stamp.Path)
+			} else {
+				panic("unsupported proto")
+			}
+		}
+
+		// do not generate anything when displaying
+		return
+	}
 
 	if (doh && dnscrypt) || (!doh && !dnscrypt) {
 		fmt.Fprintf(os.Stderr, "ERROR: either --doh or --dnscrypt should be specified\n")
 		os.Exit(1)
 	}
 
-	if port != 0 && port != 443 {
+	if port != 0 && port != stamps.DefaultPort {
 		stamp.ServerAddrStr += fmt.Sprintf(":%d", port)
 	}
 
@@ -87,7 +154,7 @@ func main() {
 	if dnssec {
 		stamp.Props |= stamps.ServerInformalPropertyDNSSEC
 	}
-	if noLogs {
+	if noLog {
 		stamp.Props |= stamps.ServerInformalPropertyNoLog
 	}
 	if noFilter {
@@ -95,4 +162,14 @@ func main() {
 	}
 
 	fmt.Println(stamp.String())
+}
+
+func formatHex(b []byte) string {
+	s := strings.ToUpper(hex.EncodeToString(b))
+
+	for i := 0; i < len(s); i += 3 {
+		s = s[:i] + ":" + s[i:]
+	}
+
+	return s[1:]
 }
