7ff250120135686e37932bdb822bbb845566b66e
Frank Denis <github@pureftpd.org>
2019-01-10 22:43:43 +0100

    Install the windows service as "NT AUTHORITY\NetworkService"
    Untested
    
    Maybe
    Fixes #686

--- dnscrypt-proxy/privilege_windows.go
+++ dnscrypt-proxy/privilege_windows.go
@@ -2,4 +2,9 @@ package main
 
 import "os"
 
+func serviceStartupUserName() *string {
+	userName := "NT AUTHORITY\\NetworkService"
+	return &userName
+}
+
 func (proxy *Proxy) dropPrivilege(userStr string, fds []*os.File) {}
