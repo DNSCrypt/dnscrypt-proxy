8076e206e03e132af3fae5765ff079fb60adf353
Frank Denis <github@pureftpd.org>
2019-03-14 20:09:23 +0100

    Revert "Install the windows service as "NT AUTHORITY\NetworkService""
    This reverts commit 17db0a658f724f82df5ea5246652317accb21664.
    
    On Windows, switching to user `NT AUTHORITY\NetworkService` apparently
    breaks logging (reported by @Aland_123).

--- dnscrypt-proxy/privilege_windows.go
+++ dnscrypt-proxy/privilege_windows.go
@@ -2,9 +2,4 @@ package main
 
 import "os"
 
-func serviceStartupUserName() *string {
-	userName := "NT AUTHORITY\\NetworkService"
-	return &userName
-}
-
 func (proxy *Proxy) dropPrivilege(userStr string, fds []*os.File) {}
