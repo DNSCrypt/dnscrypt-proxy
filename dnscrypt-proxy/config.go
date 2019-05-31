b22d6dfc96b23d5f264d2602e102e27f43d34553
Frank Denis <github@pureftpd.org>
2019-05-31 11:15:59 +0200

    Send a byte to the netprobe IP only on Windows

--- dnscrypt-proxy/config.go
+++ dnscrypt-proxy/config.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"flag"
 	"fmt"
-	"net"
 	"net/http"
 	"net/url"
 	"os"
@@ -421,7 +420,7 @@ func ConfigLoad(proxy *Proxy, svcFlag *string) error {
 			netprobeTimeout = *netprobeTimeoutOverride
 		}
 	})
-	netProbe(config.NetprobeAddress, netprobeTimeout)
+	NetProbe(config.NetprobeAddress, netprobeTimeout)
 	if !config.OfflineMode {
 		if err := config.loadSources(proxy); err != nil {
 			return err
@@ -604,44 +603,4 @@ func cdLocal() {
 		return
 	}
 	os.Chdir(filepath.Dir(exeFileName))
-}
-
-func netProbe(address string, timeout int) error {
-	if len(address) <= 0 || timeout <= 0 {
-		return nil
-	}
-	remoteUDPAddr, err := net.ResolveUDPAddr("udp", address)
-	if err != nil {
-		return err
-	}
-	retried := false
-	if timeout < 0 {
-		timeout = MaxTimeout
-	} else {
-		timeout = Max(MaxTimeout, timeout)
-	}
-	for tries := timeout; tries > 0; tries-- {
-		pc, err := net.DialUDP("udp", nil, remoteUDPAddr)
-		if err == nil {
-			// Write at least 1 byte. This ensures that sockets are ready to use for writing.
-			// Windows specific: during the system startup, sockets can be created but the underlying buffers may not be setup yet. If this is the case
-			// Write fails with WSAENOBUFS: "An operation on a socket could not be performed because the system lacked sufficient buffer space or because a queue was full"
-			_, err = pc.Write([]byte{ 0 })
-		}
-		if err != nil {
-			if !retried {
-				retried = true
-				dlog.Notice("Network not available yet -- waiting...")
-			}
-			dlog.Debug(err)
-			time.Sleep(1 * time.Second)
-			continue
-		}
-		pc.Close()
-		dlog.Notice("Network connectivity detected")
-		return nil
-	}
-	es := "Timeout while waiting for network connectivity"
-	dlog.Error(es)
-	return errors.New(es)
 }
---
+++ dnscrypt-proxy/netprobe_others.go
@@ -0,0 +1,45 @@
+// +build !windows
+
+package main
+
+import (
+	"errors"
+	"net"
+	"time"
+
+	"github.com/jedisct1/dlog"
+)
+
+func NetProbe(address string, timeout int) error {
+	if len(address) <= 0 || timeout <= 0 {
+		return nil
+	}
+	remoteUDPAddr, err := net.ResolveUDPAddr("udp", address)
+	if err != nil {
+		return err
+	}
+	retried := false
+	if timeout < 0 {
+		timeout = MaxTimeout
+	} else {
+		timeout = Max(MaxTimeout, timeout)
+	}
+	for tries := timeout; tries > 0; tries-- {
+		pc, err := net.DialUDP("udp", nil, remoteUDPAddr)
+		if err != nil {
+			if !retried {
+				retried = true
+				dlog.Notice("Network not available yet -- waiting...")
+			}
+			dlog.Debug(err)
+			time.Sleep(1 * time.Second)
+			continue
+		}
+		pc.Close()
+		dlog.Notice("Network connectivity detected")
+		return nil
+	}
+	es := "Timeout while waiting for network connectivity"
+	dlog.Error(es)
+	return errors.New(es)
+}
