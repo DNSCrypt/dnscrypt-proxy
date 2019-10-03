2f2c75dd79e7bf4d1d5935055a45a40aba42164d
Markus Linnala <markus.linnala@gmail.com>
2019-10-03 15:32:32 +0300

    change: config: handle NetProbe fatal error and run only if not offline
    All errors returned from NetProbe are managed as fatal later.
    Decide, connection issues are not fatal but bad configuration is.
    Without this configuration errors are silently ignored here.

--- dnscrypt-proxy/netprobe_windows.go
+++ dnscrypt-proxy/netprobe_windows.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"net"
 	"time"
 
@@ -43,7 +42,6 @@ func NetProbe(address string, timeout int) error {
 		dlog.Notice("Network connectivity detected")
 		return nil
 	}
-	es := "Timeout while waiting for network connectivity"
-	dlog.Error(es)
-	return errors.New(es)
+	dlog.Error("Timeout while waiting for network connectivity")
+	return nil
 }
