ab94e7eb8a6afc81b90fad0a751ab4e3e3735060
Markus Linnala <markus.linnala@gmail.com>
2019-10-03 16:46:37 +0300

    style: gofmt -w -s -d dnscrypt-proxy

--- dnscrypt-proxy/systemd_linux.go
+++ dnscrypt-proxy/systemd_linux.go
@@ -11,7 +11,7 @@ func (proxy *Proxy) SystemDListeners() error {
 	files := activation.Files(true)
 
 	if len(files) > 0 {
-		if (len(proxy.userName) > 0 || proxy.child) {
+		if len(proxy.userName) > 0 || proxy.child {
 			dlog.Fatal("Systemd activated sockets are incompatible with privilege dropping. Remove activated sockets and fill `listen_addresses` in the dnscrypt-proxy configuration file instead.")
 		}
 		dlog.Warn("Systemd sockets are untested and unsupported - use at your own risk")
