f3fe1edf1ec9748718e966ae886f78e9927f6caf
Alison Winters <alisonatwork@outlook.com>
2019-10-27 19:55:47 +0800

    move ConfigLoad into Start function when running as a service

--- dnscrypt-proxy/config.go
+++ dnscrypt-proxy/config.go
@@ -211,7 +211,7 @@ func findConfigFile(configFile *string) (string, error) {
 	return path.Join(pwd, *configFile), nil
 }
 
-func ConfigLoad(proxy *Proxy, svcFlag *string) error {
+func ConfigLoad(proxy *Proxy) error {
 	version := flag.Bool("version", false, "print current proxy version")
 	resolve := flag.String("resolve", "", "resolve a name using system libraries")
 	list := flag.Bool("list", false, "print the list of available resolvers for the enabled filters")
@@ -225,9 +225,6 @@ func ConfigLoad(proxy *Proxy, svcFlag *string) error {
 
 	flag.Parse()
 
-	if *svcFlag == "stop" || *svcFlag == "uninstall" {
-		return nil
-	}
 	if *version {
 		fmt.Println(AppVersion)
 		os.Exit(0)
