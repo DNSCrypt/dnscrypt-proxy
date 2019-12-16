10f33d39fc38e97dca46b1e2fac301a9d3a57a46
Frank <j@kiky.lan>
2019-12-16 20:02:53 +0100

    Update deps

--- vendor/github.com/kardianos/service/service_unix.go
+++ vendor/github.com/kardianos/service/service_unix.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a zlib-style
 // license that can be found in the LICENSE file.
 
-// +build linux darwin
+// +build linux darwin solaris aix
 
 package service
 
