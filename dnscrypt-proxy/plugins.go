daf6d5881d03242ca546249b90f6a6e5d7d3660d
Frank Denis <github@pureftpd.org>
2019-12-17 18:54:49 +0100

    The default return code must be PASS

--- dnscrypt-proxy/plugins.go
+++ dnscrypt-proxy/plugins.go
@@ -226,7 +226,7 @@ type Plugin interface {
 func NewPluginsState(proxy *Proxy, clientProto string, clientAddr *net.Addr, start time.Time) PluginsState {
 	return PluginsState{
 		action:                           PluginsActionForward,
-		returnCode:                       PluginsReturnCodeForward,
+		returnCode:                       PluginsReturnCodePass,
 		maxPayloadSize:                   MaxDNSUDPPacketSize - ResponseOverhead,
 		clientProto:                      clientProto,
 		clientAddr:                       clientAddr,
