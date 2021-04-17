7b7fa8b4524e36ca62210184c9724ed414c99148
Christopher Wood <caw@heapingbits.net>
2021-04-16 17:51:26 -0700

    Synchronously update the target configuration upon failure.

--- dnscrypt-proxy/proxy.go
+++ dnscrypt-proxy/proxy.go
@@ -699,7 +699,15 @@ func (proxy *Proxy) processIncomingQuery(clientProto string, serverProto string,
 					}
 				} else if responseCode == 401 {
 					dlog.Notice("Forcing key update")
-					go proxy.serversInfo.refresh(proxy)
+					for _, registeredServer := range proxy.serversInfo.registeredServers {
+						if registeredServer.name == serverInfo.Name {
+							if err = proxy.serversInfo.refreshServer(proxy, registeredServer.name, registeredServer.stamp); err != nil {
+								// Failed to refresh the proxy server information.
+								dlog.Notice("Key update failed")
+							}
+							break
+						}
+					}
 					response = nil
 				} else {
 					dlog.Error("Failed to receive successful response")
