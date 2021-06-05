2cf29f9fab360a1ee179fb8b10efd51a686fd05b
Frank Denis <github@pureftpd.org>
2021-06-05 17:04:35 +0200

    CI: check the tests after running them
    The ODoH tests don't seem to pass.

--- .ci/ci-test.sh
+++ .ci/ci-test.sh
@@ -131,11 +131,6 @@ section
 t || grep -Fq 'tracker.debian.org' allowed-names.log || fail
 t || grep -Fq '*.tracker.debian' allowed-names.log || fail
 
-if [ -s error.log ]; then
-    cat *.log
-    exit 1
-fi
-
 section
 ../dnscrypt-proxy/dnscrypt-proxy -loglevel 3 -config test3-dnscrypt-proxy.toml -pidfile /tmp/dnscrypt-proxy.pidfile &
 sleep 5
@@ -168,3 +163,8 @@ t || dig -p${DNS_PORT} A cloudflare.com @127.0.0.1 | grep -Fq "NOERROR" || fail
 
 kill $(cat /tmp/odoh-proxied.pidfile)
 sleep 5
+
+if [ -s error.log ]; then
+    cat *.log
+    exit 1
+fi
