5fedbe4c6e5795551c62c2a5891e62434c4b04ea
Frank Denis <github@pureftpd.org>
2021-09-23 19:16:26 +0200

    // +build -> //go:build

--- dnscrypt-proxy/fuzzing_test.go
+++ dnscrypt-proxy/fuzzing_test.go
@@ -1,3 +1,4 @@
+//go:build gofuzzbeta
 // +build gofuzzbeta
 
 package main
--- dnscrypt-proxy/setsockopts_others.go
+++ dnscrypt-proxy/setsockopts_others.go
@@ -1,3 +1,4 @@
+//go:build !freebsd && !openbsd && !windows && !darwin && !linux
 // +build !freebsd,!openbsd,!windows,!darwin,!linux
 
 package main
