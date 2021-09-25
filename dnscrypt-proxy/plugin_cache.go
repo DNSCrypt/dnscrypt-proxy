969cabee282cf3e6cc489f82f621a09701db717d
Frank Denis <124872+jedisct1@users.noreply.github.com>
2021-09-25 19:41:04 +0200

    Revert "Update plugin_cache.go (#1900)"
    This reverts commit 2a3e59c4bfd17ef77a3b0cc92d93051dbbaafab5.

--- dnscrypt-proxy/plugin_cache.go
+++ dnscrypt-proxy/plugin_cache.go
@@ -86,7 +86,6 @@ func (plugin *PluginCache) Eval(pluginsState *PluginsState, msg *dns.Msg) error 
 	synth.Question = msg.Question
 
 	if time.Now().After(cached.expiration) {
-		updateTTL(synth, time.Now().Add(30 * time.Second))
 		pluginsState.sessionData["stale"] = synth
 		return nil
 	}
