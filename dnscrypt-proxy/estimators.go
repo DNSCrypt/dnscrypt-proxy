9373cc71625456f9012d0186b8393d208bca4666
Frank Denis <github@pureftpd.org>
2022-02-20 23:40:32 +0100

    Use SimpleEWMA for the question size estimator

--- dnscrypt-proxy/estimators.go
+++ dnscrypt-proxy/estimators.go
@@ -6,10 +6,6 @@ import (
 	"github.com/jedisct1/ewma"
 )
 
-const (
-	SizeEstimatorEwmaDecay = 100.0
-)
-
 type QuestionSizeEstimator struct {
 	sync.RWMutex
 	minQuestionSize int
@@ -17,7 +13,7 @@ type QuestionSizeEstimator struct {
 }
 
 func NewQuestionSizeEstimator() QuestionSizeEstimator {
-	return QuestionSizeEstimator{minQuestionSize: InitialMinQuestionSize, ewma: ewma.NewMovingAverage(SizeEstimatorEwmaDecay)}
+	return QuestionSizeEstimator{minQuestionSize: InitialMinQuestionSize, ewma: &ewma.SimpleEWMA{}}
 }
 
 func (questionSizeEstimator *QuestionSizeEstimator) MinQuestionSize() int {
