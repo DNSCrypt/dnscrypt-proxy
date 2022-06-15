412a24e1bcbc06960b4a6630e19df0613a9e4fcf
YX Hao <lifenjoiner@163.com>
2022-06-15 15:01:58 +0800

    Keep error messages the same

--- dnscrypt-proxy/serversInfo.go
+++ dnscrypt-proxy/serversInfo.go
@@ -467,8 +467,9 @@ func route(proxy *Proxy, name string, serverProto stamps.StampProtoType) (*Relay
 		}
 	}
 	if len(relayStamps) == 0 {
-		dlog.Warnf("Empty relay set for [%v]", name)
-		return nil, fmt.Errorf("Invalid relay set for server [%v]", name)
+		err := fmt.Errorf("Invalid relay set for server [%v]", name)
+		dlog.Warn(err)
+		return nil, err
 	}
 	var relayCandidateStamp *stamps.ServerStamp
 	if !wildcard || len(relayStamps) == 1 {
