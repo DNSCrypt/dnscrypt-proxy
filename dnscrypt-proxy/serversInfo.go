72a602577a1dd3e82247ee4ba16e16396b4393e8
lifenjoiner <lifenjoiner@163.com>
2022-06-15 19:16:06 +0800

    Raise error for invalid relay (#2128)
    * Raise error for invalid relay
    
    * Keep error messages the same
    
    * Distinguish this from validation failed

--- dnscrypt-proxy/serversInfo.go
+++ dnscrypt-proxy/serversInfo.go
@@ -467,8 +467,9 @@ func route(proxy *Proxy, name string, serverProto stamps.StampProtoType) (*Relay
 		}
 	}
 	if len(relayStamps) == 0 {
-		dlog.Warnf("Empty relay set for [%v]", name)
-		return nil, nil
+		err := fmt.Errorf("Non-existent relay set for server [%v]", name)
+		dlog.Warn(err)
+		return nil, err
 	}
 	var relayCandidateStamp *stamps.ServerStamp
 	if !wildcard || len(relayStamps) == 1 {
