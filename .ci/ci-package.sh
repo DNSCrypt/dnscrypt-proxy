6319120c4bdfd046da0bd0067fc7d6fb86dae837
Frank Denis <github@pureftpd.org>
2025-01-10 23:42:41 +0100

    Update a few deps

--- .ci/ci-package.sh
+++ .ci/ci-package.sh
@@ -7,7 +7,7 @@ cd dnscrypt-proxy || exit 1
 # setup the environment
 
 sudo apt-get update -y
-sudo apt-get install -y wget wine dotnet-sdk-6.0
+sudo apt-get install -y wget wine dotnet-sdk-8.0
 sudo dpkg --add-architecture i386 && sudo apt-get update && sudo apt-get install -y wine32
 
 sudo apt-get install -y unzip
@@ -16,13 +16,13 @@ export WINEPREFIX="$HOME"/.wine32
 export WINEARCH=win32
 export WINEDEBUG=-all
 
-wget https://dl.winehq.org/wine/wine-mono/8.1.0/wine-mono-8.1.0-x86.msi
+wget https://dl.winehq.org/wine/wine-mono/9.4.0/wine-mono-9.4.0-x86.msi
 WINEPREFIX="$HOME/.wine32" WINEARCH=win32 wineboot --init
-WINEPREFIX="$HOME/.wine32" WINEARCH=win32 wine msiexec /i wine-mono-8.1.0-x86.msi
+WINEPREFIX="$HOME/.wine32" WINEARCH=win32 wine msiexec /i wine-mono-9.4.0-x86.msi
 
 mkdir "$HOME"/.wine32/drive_c/temp
 mkdir -p "$HOME"/.wine/drive_c/temp
-wget https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/wix311-binaries.zip -nv -O wix.zip
+wget https://github.com/wixtoolset/wix3/releases/download/wix3141rtm/wix314-binaries.zip -nv -O wix.zip
 
 unzip wix.zip -d "$HOME"/wix
 rm -f wix.zip
@@ -52,7 +52,6 @@ for arch in x64 x86; do
 
   wine candle.exe -dVersion="$version" -dPlatform=$arch -dPath=build\\$binpath -arch $arch wixproj\\dnscrypt.wxs -out build\\dnscrypt-$arch.wixobj
   wine light.exe -out build\\dnscrypt-proxy-$arch-"$version".msi build\\dnscrypt-$arch.wixobj -sval
-
 done
 
 cd "$builddir" || exit
