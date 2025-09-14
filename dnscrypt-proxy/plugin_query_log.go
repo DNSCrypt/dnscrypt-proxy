128c577ef95e2ecb945b6e31281d8728596da176
YX Hao <lifenjoiner@163.com>
2025-09-14 08:27:23 +0800

    Increase log timeout limit

--- dnscrypt-proxy/plugin_query_log.go
+++ dnscrypt-proxy/plugin_query_log.go
@@ -85,8 +85,10 @@ func (plugin *PluginQueryLog) Eval(pluginsState *PluginsState, msg *dns.Msg) err
 	}
 
 	// Cap at timeout to handle system sleep/suspend
-	if requestDuration > pluginsState.timeout {
-		requestDuration = pluginsState.timeout
+	// Max: UDP + TCP, Dial + (write + read)
+	triedUDPTCPTimeout := 4 * pluginsState.timeout
+	if requestDuration > triedUDPTCPTimeout {
+		requestDuration = triedUDPTCPTimeout
 	}
 	var line string
 	if plugin.format == "tsv" {
