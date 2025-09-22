40d23edb843afec31d81189f86535218e86c412c
Frank Denis <github@pureftpd.org>
2025-09-22 23:23:30 +0200

    Directly check that http2Transport != nil

--- dnscrypt-proxy/xtransport.go
+++ dnscrypt-proxy/xtransport.go
@@ -281,7 +281,7 @@ func (xTransport *XTransport) rebuildTransport() {
 		}
 	}
 	transport.TLSClientConfig = &tlsClientConfig
-	if http2Transport, err := http2.ConfigureTransports(transport); err == nil {
+	if http2Transport, _ := http2.ConfigureTransports(transport); http2Transport == nil {
 		http2Transport.ReadIdleTimeout = timeout
 		http2Transport.AllowHTTP = false
 	}
