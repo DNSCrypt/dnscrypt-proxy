a015a82dd4aff137b38ad065a282322552c6eb09
danny30au <thelakesclub@gmail.com>
2025-12-31 10:57:23 +1100

    Update plugin_forward.go

--- dnscrypt-proxy/plugin_forward.go
+++ dnscrypt-proxy/plugin_forward.go
@@ -112,8 +112,8 @@ func (plugin *PluginForward) parseForwardFile(lines string) (bool, []PluginForwa
     requiresDHCP := false
     forwardMap := []PluginForwardEntry{}
 
-    for lineNo, line := range strings.Split(lines, "
-") {
+    for lineNo, line := range strings.Split(lines, ") 
+       {
         line = TrimAndStripInlineComments(line)
         if len(line) == 0 {
             continue
