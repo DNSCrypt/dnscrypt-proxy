235948c0fb99f52abaa01d475df093c868169c93
danny30au <thelakesclub@gmail.com>
2026-01-01 21:10:22 +1100

    Update privilege_linux.go

--- dnscrypt-proxy/privilege_linux.go
+++ dnscrypt-proxy/privilege_linux.go
@@ -1,91 +1,99 @@
 package main
 
 import (
-	"os"
-	"os/exec"
-	"os/user"
-	"path/filepath"
-	"runtime"
-	"strconv"
-	"syscall"
+    "os"
+    "os/user"
+    "strconv"
 
-	"golang.org/x/sys/unix"
-
-	"github.com/jedisct1/dlog"
+    "github.com/jedisct1/dlog"
+    "golang.org/x/sys/unix"
 )
 
 func (proxy *Proxy) dropPrivilege(userStr string, fds []*os.File) {
-	if os.Geteuid() != 0 {
-		dlog.Fatal("Root privileges are required in order to switch to a different user. Maybe try again with 'sudo'")
-	}
-	userInfo, err := user.Lookup(userStr)
-	args := os.Args
+    if os.Geteuid() != 0 {
+        dlog.Fatal("Root privileges are required in order to switch to a different user. Maybe try again with 'sudo'")
+    }
 
-	if err != nil {
-		uid, err2 := strconv.Atoi(userStr)
-		if err2 != nil || uid <= 0 {
-			dlog.Fatalf(
-				"Unable to retrieve any information about user [%s]: [%s] - Remove the user_name directive from the configuration file in order to avoid identity switch",
-				userStr,
-				err,
-			)
-		}
-		dlog.Warnf(
-			"Unable to retrieve any information about user [%s]: [%s] - Switching to user id [%v] with the same group id, as [%v] looks like a user id. But you should remove or fix the user_name directive in the configuration file if possible",
-			userStr,
-			err,
-			uid,
-			uid,
-		)
-		userInfo = &user.User{Uid: userStr, Gid: userStr}
-	}
-	uid, err := strconv.Atoi(userInfo.Uid)
-	if err != nil {
-		dlog.Fatal(err)
-	}
-	gid, err := strconv.Atoi(userInfo.Gid)
-	if err != nil {
-		dlog.Fatal(err)
-	}
-	execPath, err := exec.LookPath(args[0])
-	if err != nil {
-		dlog.Fatalf("Unable to get the path to the dnscrypt-proxy executable file: [%s]", err)
-	}
-	path, err := filepath.Abs(execPath)
-	if err != nil {
-		dlog.Fatal(err)
-	}
+    // Optimization: Resolve UID/GID early to avoid string processing overhead
+    uid, gid := 0, 0
+    userInfo, err := user.Lookup(userStr)
 
-	if err := ServiceManagerReadyNotify(); err != nil {
-		dlog.Fatal(err)
-	}
+    if err == nil {
+        // Fast path: User found in system DB
+        u, _ := strconv.Atoi(userInfo.Uid)
+        g, _ := strconv.Atoi(userInfo.Gid)
+        uid, gid = u, g
+    } else {
+        // Fallback: Treat string as numeric ID directly
+        val, err2 := strconv.Atoi(userStr)
+        if err2 != nil || val <= 0 {
+            dlog.Fatalf(
+                "Unable to retrieve information about user [%s]: [%s] - Remove the user_name directive from the configuration file in order to avoid identity switch",
+                userStr,
+                err,
+            )
+        }
+        uid, gid = val, val
+        dlog.Warnf(
+            "Unable to retrieve information about user [%s]: [%s] - Switching to user id [%v] with the same group id. You should fix the user_name directive in the configuration file if possible",
+            userStr,
+            err,
+            uid,
+        )
+    }
 
-	args = append(args, "-child")
+    // Optimization: Use os.Executable instead of LookPath to avoid PATH traversal
+    path, err := os.Executable()
+    if err != nil {
+        dlog.Fatalf("Unable to get executable path: [%s]", err)
+    }
 
-	dlog.Notice("Dropping privileges")
+    if err := ServiceManagerReadyNotify(); err != nil {
+        dlog.Fatal(err)
+    }
 
-	runtime.LockOSThread()
-	if _, _, rcode := syscall.RawSyscall(syscall.SYS_SETGROUPS, uintptr(0), uintptr(0), 0); rcode != 0 {
-		dlog.Fatalf("Unable to drop additional groups: [%s]", rcode.Error())
-	}
-	if _, _, rcode := syscall.RawSyscall(syscall.SYS_SETGID, uintptr(gid), 0, 0); rcode != 0 {
-		dlog.Fatalf("Unable to drop group privileges: [%s]", rcode.Error())
-	}
-	if _, _, rcode := syscall.RawSyscall(syscall.SYS_SETUID, uintptr(uid), 0, 0); rcode != 0 {
-		dlog.Fatalf("Unable to drop user privileges: [%s]", rcode.Error())
-	}
-	for i, fd := range fds {
-		if fd.Fd() >= InheritedDescriptorsBase {
-			dlog.Fatal("Duplicated file descriptors are above base")
-		}
-		if err := unix.Dup2(int(fd.Fd()), int(InheritedDescriptorsBase+uintptr(i))); err != nil {
-			dlog.Fatalf("Unable to clone file descriptor: [%s]", err)
-		}
-		if _, err := unix.FcntlInt(fd.Fd(), unix.F_SETFD, unix.FD_CLOEXEC); err != nil {
-			dlog.Fatalf("Unable to set the close on exec flag: [%s]", err)
-		}
-	}
-	err = unix.Exec(path, args, os.Environ())
-	dlog.Fatalf("Unable to reexecute [%s]: [%s]", path, err)
-	os.Exit(1)
+    // Optimization: Pre-allocate slice capacity to prevent resize allocations
+    args := make([]string, 0, len(os.Args)+1)
+    args = append(args, os.Args...)
+    args = append(args, "-child")
+
+    dlog.Notice("Dropping privileges")
+
+    // Optimization: Use unix package helpers instead of RawSyscall for safety and simplicity
+    // These calls have negligible overhead compared to the process switch cost
+    if err := unix.Setgroups([]int{}); err != nil {
+        dlog.Fatalf("Unable to drop additional groups: [%s]", err)
+    }
+    if err := unix.Setgid(gid); err != nil {
+        dlog.Fatalf("Unable to drop group privileges: [%s]", err)
+    }
+    if err := unix.Setuid(uid); err != nil {
+        dlog.Fatalf("Unable to drop user privileges: [%s]", err)
+    }
+
+    // Optimization: Streamlined file descriptor handling
+    baseFd := int(InheritedDescriptorsBase)
+    for i, fd := range fds {
+        srcFd := int(fd.Fd())
+        dstFd := baseFd + i
+
+        if srcFd >= baseFd {
+            dlog.Fatal("Duplicated file descriptors are above base")
+        }
+
+        // unix.Dup2 handles closing dstFd atomically if it's already open
+        if err := unix.Dup2(srcFd, dstFd); err != nil {
+            dlog.Fatalf("Unable to clone file descriptor: [%s]", err)
+        }
+
+        // Ensure the source descriptor closes on exec, keeping only the destination open
+        if _, err := unix.FcntlInt(uintptr(srcFd), unix.F_SETFD, unix.FD_CLOEXEC); err != nil {
+             dlog.Fatalf("Unable to set the close on exec flag: [%s]", err)
+        }
+    }
+
+    // Final Exec replaces the process image
+    err = unix.Exec(path, args, os.Environ())
+    dlog.Fatalf("Unable to reexecute [%s]: [%s]", path, err)
+    os.Exit(1)
 }
