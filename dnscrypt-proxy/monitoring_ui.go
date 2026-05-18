541a059d1d1b09282e484da3376471228dac3100
lifenjoiner <lifenjoiner@163.com>
2026-05-18 08:16:39 +0000

    Don't cache dashboard HTML pages (#3183)
    Fix #3181

--- dnscrypt-proxy/monitoring_ui.go
+++ dnscrypt-proxy/monitoring_ui.go
@@ -1064,8 +1064,9 @@ func (ui *MonitoringUI) handleRoot(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Serve the main dashboard page - cache for 5 minutes since template is static
-	setStaticCacheHeaders(w, 300)
+	// No cache, no heartbeat fetching. You will need to log in again after restarting your browser.
+	setDynamicCacheHeaders(w)
+
 	w.Header().Set("Content-Type", "text/html")
 	w.Write([]byte(MainHTMLTemplate))
 }
